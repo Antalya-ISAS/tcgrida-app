0c6643acc0daecadb79f47285379f6c7f8d5814a
sefikaozturk <s.nurozturk2@gmail.com>
2021-06-10 17:56:32 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from PyQt5.QtGui import QPixmap, QImage, QIcon
 from PyQt5.QtWidgets import QApplication, QLabel, QSplitter, QPushButton, QVBoxLayout, QWidget, QHBoxLayout, QFrame
 from PyQt5.QtCore import QTimer, Qt
 import cv2
-import serialVaules
 
 # https://stackoverflow.com/questions/67027784/how-to-open-mainwindow-from-a-splashscreen-without-button-in-pyqt5
 # https://learndataanalysis.org/source-code-create-a-modern-style-flash-screen-pyqt5-tutorial/
@@ -13,32 +12,47 @@ class UI_Window(QWidget):
 
     def __init__(self):
         QWidget.__init__(self)
-
+ 
+        self.setStyleSheet("background-color: black;")
+        
         # Create a timer.
         self.timer = QTimer()
         self.timer.timeout.connect(self.nextFrameSlot)
 
         # Create the main label for camera view.
         self.label = QLabel()
-        self.label.setMinimumSize(600, 300)
+        self.label.resize(self.width() - 35, 150)
+        self.label.move(0, self.height() - 100)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.showMaximized()
 
         # Load USB Camera
-        self.openCamUSB()
+        self.openCamPC()
 
         # Create buttons
         btnOpen = QPushButton("Bilgisayar kamerasını seç")
-        btnOpen.setMinimumSize(25,35)
+        btnOpen.setMinimumSize(25, 35)
         btnOpen.setStyleSheet('''
             QPushButton { 
                 background-color: #006BFF; 
                 color: #93deed; 
                 font-size: 15px;
+                border-style:none;
+                border:1px solid #3f3f3f; 
+                padding:5px;
+                min-height:20px;
+                border-radius:15px;
+
             }
-                              
             QPushButton:hover { 
                 background-color: #00ABFF; 
                 color: #93deed; 
                 font-size: 15px;
+                border-style:none;
+                border:1px solid #3f3f3f; 
+                padding:5px;
+                min-height:20px;
+                border-radius:15px;
             }
         ''')
 
@@ -49,12 +63,22 @@ class UI_Window(QWidget):
                 background-color: #006BFF; 
                 color: #93deed; 
                 font-size: 15px;
-            }
+                border-style:none;
+                border:1px solid #3f3f3f; 
+                padding:5px;
+                min-height:20px;
+                border-radius:15px;
 
+            }
             QPushButton:hover { 
                 background-color: #00ABFF; 
                 color: #93deed; 
                 font-size: 15px;
+                border-style:none;
+                border:1px solid #3f3f3f; 
+                padding:5px;
+                min-height:20px;
+                border-radius:15px;
             }
         ''')
 
@@ -66,21 +90,7 @@ class UI_Window(QWidget):
         # Buttons on the top half
         button_layout = QHBoxLayout()
         button_layout.addWidget(btnOpen)
-
         button_layout.addWidget(btnClose)
-        # Bottom half of the screen
-        main_layout = QHBoxLayout()
-        main_layout.addWidget(self.label)
-        # Right half of main_layout
-        info_layout = QVBoxLayout()
-
-        '''
-        SİLDİĞİM LOADING FONKSIYONU İÇİN OLAN KISIM:
-        self.counter = 0
-        self.n = 300 # total instances
-        self.timerLoad = QTimer()
-        self.timerLoad.timeout.connect(self.loading)
-        self.timerLoad.start(30)'''
 
         self.frame = QFrame()
 
@@ -90,43 +100,16 @@ class UI_Window(QWidget):
         self.labelTitle.move(0, 40)  # x, y
         self.labelTitle.setText('<strong>tcGrida App</strong>')
         self.labelTitle.setAlignment(Qt.AlignCenter)
-        info_layout.addWidget(self.labelTitle)
 
-        self.labelDescription = QLabel(self.frame)
-        self.labelDescription.resize(self.width() - 10, 50)
-        self.labelDescription.move(0, self.labelTitle.height())
-        self.labelDescription.setObjectName('LabelDesc')
-        self.labelDescription.setText('<strong>Basınç: </strong>' + serialVaules.returnPressure())
-        self.labelDescription.setAlignment(Qt.AlignCenter)
-        info_layout.addWidget(self.labelDescription)
-
-        self.labelLoading = QLabel(self.frame)
-        self.labelLoading.resize(self.width() - 10, 50)
-        self.labelLoading.move(0, self.labelDescription.y() + 70)
-        self.labelLoading.setObjectName('LabelDesc')
-        self.labelLoading.setAlignment(Qt.AlignCenter)
-        self.labelLoading.setText('<strong>Nem: </strong>' + serialVaules.returnHumidity())
-        info_layout.addWidget(self.labelLoading)
-
-        # Create a logo.
-        self.label_logo = QLabel(self.frame)
-        self.label_logo.resize(self.width() - 10, 50)  # önceden (self.width() - 10, 50) idi
-        self.label_logo.move(0, self.labelLoading.y() + 70)
-        self.label_logo.setObjectName('LabelLogo')
-        logo_pixmap = QPixmap('logo.png')
-        self.label_logo.setPixmap(logo_pixmap)
-        self.label_logo.setAlignment(Qt.AlignCenter)
-        info_layout.addWidget(self.label_logo)
-
-        # Add them into each other
+        # Bottom half of the screen
+        layout.addWidget(self.labelTitle)
         layout.addLayout(button_layout)
-        layout.addLayout(main_layout)
-        main_layout.addLayout(info_layout)
+        layout.addWidget(self.label)
 
         # Set the layout
         self.setLayout(layout)
         self.setWindowTitle("tcGrida App")
-        self.setMinimumSize(700,350)
+        self.setMinimumSize(700, 350)
         self.setWindowIcon(QIcon('logo.png'))
 
         self.showMaximized()
@@ -134,23 +117,27 @@ class UI_Window(QWidget):
     def openCamPC(self):
         self.vc = cv2.VideoCapture(0)
         # vc.set(5, 30)  #set FPS
-        self.vc.set(3, 800)  # set width
-        self.vc.set(4, 650)  # set height
+        self.vc.set(3, 4000)  # set width
+        self.vc.set(4, 1500)  # set height
         self.timer.start(round(1000. / 24))
 
     def openCamUSB(self):
         self.vc = cv2.VideoCapture(1)
         # vc.set(5, 30)  #set FPS
-        self.vc.set(3, 800)  # set width
-        self.vc.set(4, 650)  # set height
+        self.vc.set(3, 4000)  # set width
+        self.vc.set(4, 1500)  # set height
         self.timer.start(round(1000. / 24))
 
     # https://stackoverflow.com/questions/41103148/capture-webcam-video-using-pyqt
     def nextFrameSlot(self):
         rval, frame = self.vc.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image = QImage(frame, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+        resize = cv2.cv2.resize(frame, (1300, 1000))
+        image = QImage(resize, resize.shape[1], resize.shape[0], QImage.Format_RGB888)
         pixmap = QPixmap.fromImage(image)
+       # logo_pixmap = cv2.cv2.imread('logo.png')
+       # dst = cv2.addWeighted(frame,1.0,logo_pixmap,0.7,0) #bu satır düzeltilecek
+
         self.label.setPixmap(pixmap)
 
 
@@ -160,20 +147,17 @@ def main():
 
     app.setStyleSheet('''
         #LabelTitle {
-            font-size: 60px;
+            font-size: 45px;
             color: #006BFF;
         }
-
         #LabelDesc {
             font-size: 30px;
-            color: #2F4454;
-        }
-
-        QFrame {
-            background-color: #c2ced1;
             color: #c2ced1;
         }
-
+        QFrame {
+            background-color: #2F4454;
+            color: #2F4454;
+        }
     ''')
 
     ex = UI_Window()

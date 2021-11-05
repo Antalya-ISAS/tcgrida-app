d6aca1c39c7b3ea080f577408451c010fe97dd20
egeakman <egeakmanegeakman@hotmail.com>
2021-11-05 21:53:24 +0300

    Remove test lines

--- app/main.py
+++ app/main.py
@@ -11,7 +11,6 @@ from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import QPropertyAnimation, QMetaObject, QSize, Qt, QTimer
 from PyQt5.QtWidgets import *
 
-# Testing git (Remove later) ADAMSIN
 
 # GUI FILE
 from app_modules import *

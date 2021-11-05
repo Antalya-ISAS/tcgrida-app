cb10b49c564976f3cb3e982aa214f0d66a198f19
Arda Sak <ardasak434@gmail.com>
2021-11-05 21:23:14 +0300

    Testing git

--- app/main.py
+++ app/main.py
@@ -11,6 +11,8 @@ from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import QPropertyAnimation, QMetaObject, QSize, Qt, QTimer
 from PyQt5.QtWidgets import *
 
+# Testing git (Remove later)
+
 # GUI FILE
 from app_modules import *
 
